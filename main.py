ff416bb2687e1103f26195c4ddd73fab76ee78da
notaspect372 <notaspect372@gmail.com>
2025-02-01 18:16:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -213,7 +213,8 @@ def scrape_multiple_urls(base_urls):
 
 # Example usage
 base_urls = [
-    "https://www.privateproperty.co.za/for-sale/gauteng/centurion/32"
+    "https://www.privateproperty.co.za/for-sale/gauteng/gauteng-south/812",
+    "https://www.privateproperty.co.za/for-sale/gauteng/midrand/24"
 ]
 
 scrape_multiple_urls(base_urls)
